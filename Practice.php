5d661b40a15863b7dd5a0ccb591f533d81d64279
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-09-18 09:23:13 +0000

    practice(9/18)

--- Practice.php
+++ Practice.php
@@ -5,5 +5,26 @@ echo 'こんにちは';
 <?php
 $who = 'php 7';
 echo "こんにちは、", $who;
-echo PHP_EOL;
 ?>
+
+<?php
+echo "みなさん, ";
+echo "こんにちは";
+echo PHP_EOL;
+echo "みなさん, "; echo "こんばんは";
+?>
+
+<?php
+echo "こんにちは、";
+ECHO "こんばんは";
+?>
+
+<?php
+$mycolor = "red";
+$MYCOLOR = "RED";
+
+echo $mycolor;
+echo "、";
+echo $MYCOLOR;
+?>
+
