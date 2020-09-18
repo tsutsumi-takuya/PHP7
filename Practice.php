af92441e921f4a1cd4da3441a2a86e731aabfbe7
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-09-18 09:41:07 +0000

    practice(9/18)

--- Practice.php
+++ Practice.php
@@ -28,3 +28,46 @@ echo "、";
 echo $MYCOLOR;
 ?>
 
+<?php
+$size = "M";
+$price = "¥1200";
+
+echo $size, ",";
+echo $price, "です";
+?>
+
+<?php
+$Math = "65";
+$English = "70";
+$Science = "85";
+$total = $Math + $English + $Science;
+
+$heikin = $total / 3 ;
+
+echo "数学の点数は", $Math, "です。";
+echo PHP_EOL;
+echo "英語の点数は", $English, "です。";
+echo PHP_EOL;
+echo "科学の点数は", $Science, "です。";
+echo PHP_EOL;
+echo "三教科の平均点数は", $heikin, "です。";
+?>
+
+<?php
+$zaiko = "在庫なし";
+echo $zaiko;
+echo PHP_EOL;
+$zaiko = 5;
+echo $zaiko;
+echo PHP_EOL;
+
+const tax = 1.08;
+$prise = 1250;
+$total = $prise * tax;
+echo $total;
+echo PHP_EOL;
+
+// <br> => htmlでの表記はされないが、コンソール上では表示されたしまう
+// echo PHP_EOL; => htmlとコンソール上のどちらでも表示されない
+
+?>
