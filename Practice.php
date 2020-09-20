03f5a44c642b38efc5b073d888886e5ee5b05fd1
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-09-20 02:53:37 +0000

    practice(9/20)

--- Practice.php
+++ Practice.php
@@ -153,3 +153,141 @@ $price = "500円" * $ninzu;
 $price = $price * "1.08 消費税";
 echo "料金は{$price}円、人数は{$ninzu}人";
 ?>
+
+<?php
+$a = 0;
+$b = ++$a;
+echo "aは{$a}、bは{$b}";
+?>
+
+<?php // インクリメント/デクリメント
+$a = 0;
+$b = $a++;
+echo "aは{$a}、bは{$b}";
+?>
+
+<?php // ポストインクリメント/ポストデクリメント
+$myNum = "19";
+$myChar = "a";
+++$myNum;	// 数字の値にプラスされる
+++$myChar;	// アルファベットにプラスされる
+echo "myNumは{$myNum}、myCharは{$myChar}";
+?>
+
+<?php // 文字列結合演算子
+$who = "Bob";
+$hello = "Goodmorning ";
+$msg = $who."！".$hello;
+echo $msg;
+?>
+
+<?php // 数値と文字の連結をピリオドで行う
+$num = 19+1;
+$msg1 = $num. "番". PHP_EOL;
+$msg2 = $num. 77;
+echo $msg1;
+echo $msg2;
+?>
+
+<?php // 比較演算子
+$a = 7;
+$b =10;
+$judge1 = ($a<$b);
+$judge2 = ($a>$b);
+var_dump($judge1);
+var_dump($judge2);
+?>
+
+<?php
+$point = 11.6;
+if ($point >= 10) { // pointが10以上(10ぴったりでも可)でtrue条件
+	echo "合格";
+} else {
+	echo "不合格";
+}
+?>
+
+<?php
+$prise = 250 * ($kosuu ?? 2);
+var_dump($kosuu); // $kosuuがNULLだが計算を行って出力してくれる
+echo $price;
+?>
+
+<?php
+$judge1 = ("99" == 99); // 文字列の99と数値の99が同じかの判定
+$judge2 = ("99" != 99); // 文字列と数値の型が違う判定
+var_dump($judge1);
+var_dump($judge2);
+?>
+
+<?php
+$judge1 = ("99" === 99); // 値だけでなく型も同じかの判定
+$judge2 = ("99" !== 99); // 文字列と数値の型が違う判定
+var_dump($judge1);
+var_dump($judge2);
+?>
+
+<?php // 論理演算子
+$test1 = true;
+$test2 = false;
+$judge1 = $test1 && $test2;	// aかつbがtrueの場合
+$judge2 = $test1 || $test2; // aまたはbがtrueの場合
+$judge3 = !$test1;			// 条件の否定(aがtrueならb,bがfalseならtrue)
+var_dump($judge1);
+var_dump($judge2);
+var_dump($judge3);
+?>
+
+<?php
+$test1 = true;
+$test2 = false;
+$judge1 = ($test1 and $test2);	// &&と同義　※()が必要
+$judge2 = ($test1 or $test2); 	// ||と同義　※()が必要
+var_dump($judge1);
+var_dump($judge2);
+?>
+
+<?php
+$test1 = true;
+$test2 = false;
+$judge1 = $test1 and $test2;	// ()がないと=が優先されてしまう　※ $judge1 = $test1
+$judge2 = $test1 or $test2; 	// ()がないと=が優先されてしまう　※ $judge2 = $test1
+var_dump($judge1); // 論理値の出力(var_dump関数)
+var_dump($judge2);
+?>
+
+<?php // 三項演算子
+$a = mt_rand(0,50); // ランダムで値の指定(0~50)
+$b = mt_rand(0,50);
+$bigger = ($a>$b)? $a : $b;
+echo "大きな値は{$bigger},aは{$a}でbは{$b}";
+?>
+
+<?php
+$a = mt_rand(0,50); // ランダムで値の指定(0~50)
+$b = mt_rand(0,50);
+if ($a>$b) {
+	$bigger = $a;
+} else {
+	$bigger =$b;
+}
+echo "大きな値は{$bigger},aは{$a}でbは{$b}";
+?>
+
+<?php // キャスト演算子
+$theDate = new DateTime();
+$isAccess = (bool)$theDate; // $theDateに値が入っているのでtrue
+var_dump($isAccess);
+?>
+
+<?php
+$theDate = "";
+$isAccess = (bool)$theDate; // $theDateに値が入っていないのでfalse
+var_dump($isAccess);
+?>
+
+<?php // 型演算子
+$now = new DateTime();
+$isDate = $now instanceof DateTime;
+var_dump($isDate);
+?>
